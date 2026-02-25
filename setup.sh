f45ff4a4ac5441460a51c13644894907991dafa5
velosipedistufa <ilgamgalimov897@gmail.com>
2026-02-25 13:55:29 +0300

    added usbutils and wget, needed for printing

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ runuser -u alex scp alex@192.168.88.1:/home/alex/apearance/archiv.7z /home/alex/
 runuser -u alex 7z x archiv.7z
 sudo cp -a /home/alex/appearance/wayland-sessions/. /usr/share/wayland-sessions/
 sudo cp -a /home/alex/appearance/waybar/. /etc/xdg/waybar
-pacman -Syu --noconfirm fuzzel ffmpeg swaybg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty thunar mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch ristretto
+pacman -Syu --noconfirm fuzzel ffmpeg swaybg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty thunar mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch ristretto usbutils wget
 #pipewire-alsa should be removed as far as firefox will implement pipewire support
 runuser -u alex git clone https://aur.archlinux.org/yay.git
 cd yay
