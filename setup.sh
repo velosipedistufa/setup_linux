abef65283fa3865419c2fb093c3bdf6f76174f8f
velosipedistufa <ilgamgalimov897@gmail.com>
2026-03-10 15:52:49 +0300

    added bluetooth tools

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ runuser -u alex scp alex@192.168.88.1:/home/alex/apearance/archiv.7z /home/alex/
 runuser -u alex 7z x archiv.7z
 sudo cp -a /home/alex/appearance/wayland-sessions/. /usr/share/wayland-sessions/
 sudo cp -a /home/alex/appearance/waybar/. /etc/xdg/waybar
-pacman -Syu --noconfirm fuzzel ffmpeg swaybg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty thunar mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch ristretto usbutils wget
+pacman -Syu --noconfirm fuzzel ffmpeg swaybg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty thunar mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch ristretto usbutils wget simple-scan docker blueman bluez bluez-utils
 #pipewire-alsa should be removed as far as firefox will implement pipewire support
 runuser -u alex git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -26,6 +26,8 @@ runuser -u alex  make
 systemctl enable ly@tty2.service
 systemctl disable getty@tty2.service
 
+systemctl enable --now bluetooth
+
 #cp start-dwl.sh /home/alex/appearance
 #cp config.h /home/alex/dwl/
 #cp wallpaper.png /home/alex/appearance
@@ -40,3 +42,4 @@ systemctl --user enable --now pipewire wireplumber
 
 #for vm's
 #sudo pacman -S qemu-full virt-manager libvirt polkit dmidecode dnsmasq
+#stirling pdf should be deployed on a home server
