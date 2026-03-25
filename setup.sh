749c70fd4ff0070d2a9563ae2bff5beb6d272c76
velosipedistufa <ilgamgalimov897@gmail.com>
2026-03-25 17:25:11 +0300

    added stuff for themes

--- setup.sh
+++ setup.sh
@@ -7,14 +7,14 @@ mkdir -p /usr/share/wayland-sessions
 #runuser -u alex 7z x archiv.7z
 sudo cp -a /home/alex/appearance/wayland-sessions/. /usr/share/wayland-sessions/
 sudo cp -a /home/alex/appearance/waybar/. /etc/xdg/waybar
-pacman -Syu --noconfirm fuzzel ffmpeg swaybg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty thunar mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch ristretto usbutils wget simple-scan docker blueman bluez bluez-utils android-tools dnsutils llvm
+pacman -Syu --noconfirm fuzzel ffmpeg swaybg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty thunar mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch ristretto usbutils wget simple-scan docker blueman bluez bluez-utils android-tools dnsutils llvm gsettings-desktop-schemas
 #pipewire-alsa should be removed as far as firefox will implement pipewire support
 cd /home/alex
 runuser -u alex git clone https://aur.archlinux.org/yay.git
 cd yay
 runuser -u alex makepkg -si
 cd ..
-runuser -u alex yay -S --sudoloop wlroots0.19 wayland-protocols ly flat-remix #vimix-gtk-theme
+runuser -u alex yay -S --sudoloop wlroots0.19 wayland-protocols ly flat-remix flat-remix-gtk #vimix-gtk-theme
 
 cd /home/alex/setup_linux
 
