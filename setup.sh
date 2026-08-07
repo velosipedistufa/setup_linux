9fa2abb13a7597a30c5f7f91fd5cdb47479d46b4
velosipedistufa <ilgamgalimov897@gmail.com>
2026-08-07 14:12:56 +0300

    Added yazi, swayimg, mihomo-tui and stig instead of thunar, ristretto, throne and transmission

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ mkdir -p /usr/share/wayland-sessions
 #runuser -u alex 7z x archiv.7z
 sudo cp -a /home/alex/appearance/wayland-sessions/. /usr/share/wayland-sessions/
 sudo cp -a /home/alex/appearance/waybar/. /etc/xdg/waybar
-pacman -Syu --noconfirm fuzzel ffmpeg swaybg swayimg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty yazi mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch usbutils wget simple-scan docker blueman bluez bluez-utils android-tools dnsutils llvm gsettings-desktop-schemas udisks2 make python
+pacman -Syu --noconfirm fuzzel ffmpeg swaybg swayimg waybar pipewire wireplumber wayland libinput pkg-config pipewire-jack git cmatrix btop htop go udiskie xdg-desktop-portal xdg-desktop-portal-wlr kitty yazi transmission mousepad playerctl bluez bluez-utils jq libxss mailcap libxt gnu-free-fonts lxappearance gtk4 pipewire-alsa pipewire-v4l2 sof-firmware alsa-ucm-conf grim slurp wl-clipboard swappy gvfs 7zip neovim hyfetch usbutils wget simple-scan docker blueman bluez bluez-utils android-tools dnsutils llvm gsettings-desktop-schemas udisks2 make python
 #pipewire-alsa should be removed as far as firefox will implement pipewire support
 cd /home/alex
 runuser -u alex git clone https://aur.archlinux.org/yay.git
